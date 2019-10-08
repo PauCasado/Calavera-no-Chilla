898ac4c90a718850b00d33c08e5da5aacf833205
Paula Casado <palita1416@gmail.com>
2019-10-08 20:13:04 -0300

    view y template realizados

--- Calavera No Chilla/views/viewCategorias.php
+++ Calavera No Chilla/views/viewCategorias.php
@@ -6,14 +6,22 @@
         private $smarty;
         public function __construct() {
             $this->smarty = new Smarty();
+            $authHelper = new AuthHelper();
+
             $this->smarty->assign('titulo', 'Categorías');
             $this->smarty->assign('basehref', BASE_URL);
+            $this->smarty->assign('username', $authHelper->obternerNombreUsuario());
         }
         
         public function mostrarCategorias($categorias){
             $this->smarty->assign('categorias', $categorias);
             $this->smarty->display('templates/viewCategorias.tpl');
             
+        }
+        public function error($msgError){
+            $this->smarty->assign('msgError', $msgError);
+            $this->smarty->display('templates/viewError.tpl');
+
         }
         
     }
